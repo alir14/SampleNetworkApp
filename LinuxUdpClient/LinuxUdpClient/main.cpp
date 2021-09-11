32d1ae9145500d8c036fb91cfb8b312fdbee9465
Ali <ali_r14@yahoo.com>
2021-09-11 21:28:45 +1200

    adding linux udp client init code

---
+++ LinuxUdpClient/LinuxUdpClient/main.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+void error(const char* msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char* argv[])
+{
+    int sockfd, portNo, n;
+    struct sockaddr_in server_addr;
+    struct hostent* server;
+
+    char buffer[256];
+
+    portNo = 54000; //atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    server = gethostbyname("192.168.1.67");
+
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char*)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    bcopy((char*)server->h_addr,(char*)&server_addr.sin_addr.s_addr,server->h_length);
+    //inet_pton(AF_INET, "192.168.1.79", &server_addr.sin_addr);
+
+    server_addr.sin_port = htons(portNo);
+
+    if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+        error("ERROR connecting");
+
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 256, stdin);
+
+    int msgSize = strlen(buffer) + 1;
+    printf("message size: %i\n", msgSize);
+
+    int sendOk = sendto(sockfd, buffer, msgSize, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    //n = write(sockfd,buffer, strlen(buffer));
+
+    //if(n < 0)
+    //    error("ERROR writing to socket");
+
+    //bzero(buffer, 256);
+    //n = read(sockfd, buffer, 255);
+
+    //if (n < 0)
+    //     error("ERROR reading from socket");
+
+    //printf("%s\n", buffer);
+    close(sockfd);
+
+    return 0;
+}
+
